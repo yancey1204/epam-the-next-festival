ff9d6fb539822760667d6a124bf7f6fa420d7fed
yancey1204 <yancey1204@gmail.com>
2016-03-07 23:37:30 +0800

    get the home page working with mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // include and setup express
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 // include express handlebars (templating engine)
 var exphbs  = require('express-handlebars');
@@ -17,32 +18,58 @@ var api = require('./routes/api');
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// express middleware that parser the key-value pairs sent in the request body in the format of our choosing (e.g. json) 
+// express middleware that parser the key-value pairs sent in the request body in the format of our choosing (e.g. json)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // setup our public directory (which will serve any file stored in the 'public' directory)
 app.use(express.static('public'));
 
+app.use(function (req, res, next) {
+	res.locals.scripts = [];
+	next();
+});
+
 // respond to the get request with the home page
 app.get('/', function (req, res) {
-    res.render('home');
+	res.locals.scripts.push('/js/main.js');
+	res.render('home');
 });
+
+
+// respond to the get specific article by id
+app.get('/article/:id', function (req, res) {
+
+	var fs = require('fs');
+	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
+		if (err) throw err;
+
+		data = _.filter(JSON.parse(data), function(item) {
+			return item.id == req.params.id;
+		});
+
+		// res.json(data);
+		res.render('article',{article:data[0]});
+	});
+
+});
+
+
 
 // respond to the get request with the about page
 app.get('/about', function(req, res) {
-  res.render('about');
+	res.render('about');
 });
 
 // respond to the get request with the register page
 app.get('/register', function(req, res) {
-  res.render('register');
+	res.render('register');
 });
 
 // handle the posted registration data
 app.post('/register', function(req, res) {
 
-  // get the data out of the request (req) object 
+  // get the data out of the request (req) object
   // store the user in memory here
 
   res.redirect('/dashboard');
@@ -50,12 +77,12 @@ app.post('/register', function(req, res) {
 
 // respond to the get request with dashboard page (and pass in some data into the template / note this will be rendered server-side)
 app.get('/dashboard', function (req, res) {
-    res.render('dashboard', {
-    	stuff: [{
-		    greeting: "Hello",
-		    subject: "World!"
+	res.render('dashboard', {
+		stuff: [{
+			greeting: "Hello",
+			subject: "World!"
 		}]
-    });
+	});
 });
 
 // the api (note that typically you would likely organize things a little differently to this)
@@ -66,5 +93,5 @@ var server = require('http').createServer(app);
 
 // start the server
 server.listen(1337, '127.0.0.1', function () {
-  console.log('The Next XYZ is looking good! Open http://localhost:%d to begin.', 1337);
+	console.log('The Next XYZ is looking good! Open http://localhost:%d to begin.', 1337);
 });
--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,52 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('underscore');
+var mongoose = require('mongoose');
+
+// connect to database
+mongoose.connect('mongodb://localhost/epam');
+var Schema = mongoose.Schema;
+
+// create a Schema for Articles
+var ArticleSchema = new Schema({
+	title: String,
+	url: String,
+	image: String,
+	username: String,
+	date: Date
+});
+
+mongoose.model('Article',ArticleSchema);
+
+var Article = mongoose.model('Article');
+
+
 
 // note that typically data would NOT be loaded from the filesystem in this manner :)
 
 router.get('/articles', function(req, res, next) {
-
-	var fs = require('fs');
-	var obj;
-	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
-	  if (err) throw err;
-	  res.json(JSON.parse(data));
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Headers','X-Requested-Width');
+	console.log('mogodbServer getArticles');
+	Article.find({},null,{sort:{date:-1}},function(err,data){
+		res.json(data);
 	});
+
 });
 
 router.get('/articles/:id', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Headers','X-Requested-Width');
+	console.log('mogodbServer getArticles by id: ',req.params.id);
+	Article.findById(req.params.id,function(err,article){
+		if(!err){
+			res.json(article);
+		}
+		else{
+			res.send(404,'file not found');
+		}
+	})
 
-	var fs = require('fs');
-	var obj;
-	fs.readFile('./data/articles.json', 'utf8', function (err, data) {
-		if (err) throw err;
-
-		data = _.filter(JSON.parse(data), function(item) {
-		    return item.id == req.params.id;
-		});
-
-		res.json(data);
-	});
 });
 
 module.exports = router;
